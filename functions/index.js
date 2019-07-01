89e0c397cac6d94f475d6ed2c104bb6e582c199e
Roneet Kumar <roneetparida@gmail.com>
2019-07-01 02:16:56 -0400

    search, product info, ui fixes, side nav

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
+let $ = (ele) => document.querySelector(ele);
+
+
 let li = $('ul.link-list').getElementsByTagName('li');
 let cards = $('main').getElementsByClassName('card');
 let category = $('.selected').innerHTML.toLowerCase();
+let buttons = $('main').getElementsByTagName('button');
 
-function $(ele) {
-    return document.querySelector(ele);
-}
 
 window.onscroll = function() {
     let fab = $('.fab svg');
@@ -51,7 +52,7 @@ data.forEach(function(item) {
     <div class="card ${category}">
     <img src="${item.imgSrc}" onerror="this.src='assets/head_paper.png'">
     <a><h2> $${item.price} CAD </h2>
-    <h1> ${item.productName}</h1> </a>
+    <h1>${item.productName}</h1></a>
     <button> VIEW </button>
     </div>
     `;
@@ -75,10 +76,55 @@ $('.menu').onclick = function() {
     $('.navMenu').style.transform = 'translateX(0)';
     $('.nav-overlay').style.display = 'block';
     $('body').style.overflow = 'hidden';
+    $('.search').style.zIndex = '0';
 }
 
 $('.nav-overlay').onclick = function() {
     $('.navMenu').style.transform = 'translateX(-312px)';
     $('.nav-overlay').style.display = 'none';
     $('body').style.overflow = 'initial';
+    $('.product-info').classList.remove('isOpened');
+    if ($('#myinput').value !== '') {
+        $('.close').style.display = 'block';
+    } else {
+        $('.topbar').classList.remove('searchOpen');
+    }
+}
+
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+        $('.product-info').classList.add('isOpened');
+        $('.nav-overlay').style.display = 'block';
+        let selectedProduct = this.parentElement.querySelector('h1').innerHTML;
+        $('.search').style.zIndex = '0';
+
+        data.forEach(function(item) {
+            if (selectedProduct == item.productName) {
+                console.log(item.price);
+                const productInfo = `
+                <h4>${item.productName}</h4>
+                <h4>${item.price}</h4>
+                <img src='${item.imgSrc}'>
+                <h4>${item.category}</h4>
+                `;
+
+                $('.product-info').innerHTML = productInfo;
+            }
+        });
+    }
+}
+
+$('.search').onclick = function() {
+    $('.topbar').classList.add('searchOpen');
+    $('.nav-overlay').style.display = 'block';
+}
+
+$('.close').onclick = function(event) {
+    $('.topbar').classList.remove('searchOpen');
+    $('#myinput').value = "";
+    searchFunction();
+    $('.nav-overlay').style.display = 'none';
+    $('.close').style.display = 'none';
+    event.stopPropagation();
 }

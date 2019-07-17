0759962164dfada096f2a088d28d4f64d01cb314
Roneet Kumar <roneetparida@gmail.com>
2019-07-17 16:54:52 -0400

    cart working

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,25 @@ let colors = ['#00BCD4', '#673AB7', '#607D8B'];
 let selectedColor = colors[Math.floor(Math.random() * colors.length)];
 
 let li = $('ul.link-list').getElementsByTagName('li');
-let cards = $('main').getElementsByClassName('card');
+
 let category = $('.selected').innerHTML.toLowerCase();
-let buttons = $('main').getElementsByTagName('button');
+
+product.forEach(product => {
+    let category = product.categories.toLowerCase();
+    const card = `
+    <div class="card ${category}">
+    <img src="${product.imgSrc}" alt="${product.name}" onerror="this.src='assets/head_paper.png'" loading="lazy">
+    <a><h2> $${product.price} CAD </h2>
+    <h1>${product.name}</h1></a>
+    <button> ADD TO CART </button>
+    </div>
+    `;
+    $('main').innerHTML += card;
+});
+
+let cards = $$('main .card');
+let buttons = $$('main button');
+
 
 window.onscroll = function () {
     let fab = $('.fab svg');
@@ -24,40 +40,61 @@ window.onscroll = function () {
     }
 };
 
+const productInfoShow = () => {
+    $('.product-info').classList.add('isOpened');
+}
+
+const productInfoHide = () => {
+    $('.product-info').classList.remove('isOpened');
+}
+
+const canScroll = () => {
+    $('body').style.overflow = 'initial';
+}
+
+const canNotScroll = () => {
+    $('body').style.overflow = 'hidden';
+}
+
 const searchOpen = () => {
     $('.topbar').classList.add('searchOpen');
 }
+
 const searchClose = () => {
-    // setTimeout(function () {
     $('.topbar').classList.remove('searchOpen');
-    // }, 1355);
 }
-const sideNavClose = () => $('.side-nav').style.transform = 'translateX(-312px)';
+
 const sideNavOpen = () => {
     $('.side-nav').style.transform = 'translateX(0)';
     $('.search').style.zIndex = '0';
 }
-const loginHide = () => $('.login').style.transform = 'translate(-50%,-50%) scale(0)';
+
+const sideNavClose = () => {
+    $('.side-nav').style.transform = 'translateX(-312px)';
+}
+
 const loginShow = () => {
     $('.search').style.zIndex = '0';
     $('.login').style.transform = 'translate(-50%,-50%) scale(1)';
 }
-const productInfoHide = () => $('.product-info').classList.remove('isOpened');
-const productInfoShow = () => $('.product-info').classList.add('isOpened');
-const canScroll = () => $('body').style.overflow = 'initial';
-const canNotScroll = () => $('body').style.overflow = 'hidden';
+
+const loginHide = () => {
+    $('.login').style.transform = 'translate(-50%,-50%) scale(0)';
+}
+
+function searchInputCheck() {
+    if ($('#myinput').value !== '') {
+        $('.close').style.display = 'block';
+    } else {
+        searchClose();
+    }
+}
 
 const overlayHide = () => {
     $('.overlay').style.display = 'none';
     $('.search').style.zIndex = '1';
     $('.cart').style.zIndex = '0';
     $('.overlay').style.opacity = '0';
-    searchInputCheck();
-    sideNavClose();
-    productInfoHide();
-    canScroll();
-    loginHide();
-    closeCart();
 }
 
 
@@ -79,29 +116,37 @@ const toggleLogIn = () => {
     }
 }
 
-function searchInputCheck() {
-    if ($('#myinput').value !== '') {
-        $('.close').style.display = 'block';
-    } else {
-        searchClose();
-    }
+function logout() {
+    $('.nav-heading h1.title').innerHTML = '';
+    $('.nav-heading a h1.email').innerHTML = '';
+    $('.nav-heading a').href = '';
+    $('.topbar span').innerHTML = 'Sign Up | Log In';
+    $('ul.nav-menu .signin').style.display = 'block';
+    $('ul.nav-menu .logout').style.display = 'none';
+    $('ul.nav-menu .signin h1').innerText = 'Sign In | Sign Up';
+    $('.login h1.LoggedIn').style.display = 'flex';
+    $('.login h1.LoggedOut').style.display = 'block';
+    sideNavClose();
+    overlayHide();
+    loginHide();
+    setTimeout(function () {
+        $('.login-form').style.display = 'block';
+        $('.signup-form').style.display = 'block';
+        $('.account-info').style.display = 'none';
+    }, 500);
 }
-
 
 function searchFunction() {
     let input = document.getElementById('myinput').value.toUpperCase();
-    let productName = document.querySelectorAll('.card a');
+    let productName = $$('.card a');
 
-    productName.forEach(function (item) {
-        if (item.innerText.toUpperCase().includes(input)) {
-            console.log('hi');
-            item.parentElement.style.display = '';
-
+    productName.forEach(product => {
+        if (product.innerText.toUpperCase().includes(input)) {
+            product.parentElement.style.display = '';
         } else {
-            item.parentElement.style.display = 'none';
+            product.parentElement.style.display = 'none';
         }
     });
-    // filter();
 }
 
 function filter() {
@@ -113,28 +158,6 @@ function filter() {
         }
     }
 }
-
-function logout() {
-    sideNavClose();
-    overlayHide();
-    loginHide();
-    $('.nav-heading h1.title').innerHTML = '';
-    $('.nav-heading a h1.email').innerHTML = '';
-    $('.nav-heading a').href = '';
-    $('.topbar span').innerHTML = 'Sign Up | Log In';
-    $('ul.nav-menu .signin').style.display = 'block';
-    $('ul.nav-menu .logout').style.display = 'none';
-    $('ul.nav-menu .signin h1').innerText = 'Sign In | Sign Up';
-    setTimeout(function () {
-        $('.login-form').style.display = 'block';
-        $('.signup-form').style.display = 'block';
-        $('.account-info').style.display = 'none'
-
-    }, 500);
-    $('.login h1.LoggedIn').style.display = 'flex';
-    $('.login h1.LoggedOut').style.display = 'block';
-}
-
 
 function sortList(sortBy) {
     product = product.slice(0);
@@ -151,44 +174,64 @@ function sortList(sortBy) {
     });
 }
 
+sortList('A');
+
 function openCart() {
     $('.cart').style.zIndex = '1'
     $('.cart').classList.add('openCart');
-    $('.cart .svg-group').style.display = 'none';
+    $('.cart svg').style.display = 'none';
     $('.search').style.zIndex = '0';
-    overlayShow();
+    $('.cart span').style.display = 'none';
+    setTimeout(function () {
+        $('.cart h1').style.display = 'block';
+    }, 300);
+
+
+
+
 }
 
 function closeCart() {
     $('.cart').classList.remove('openCart');
+    $('.cart h1').style.display = '';
     setTimeout(function () {
-        $('.cart .svg-group').style.display = '';
+        $('.cart svg').style.display = '';
+        $('.cart span').style.display = '';
     }, 300);
 }
 
-$('.cart').onclick = () => openCart();
+function productInfoClose() {
+    productInfoHide();
+    canScroll();
+    overlayHide();
+}
+
+$('.cart').onclick = () => {
+    openCart();
+    overlayShow();
+    canNotScroll();
+}
+
+$('.cart').onclick = () => {
+    openCart();
+    overlayShow();
+    canNotScroll();
+}
+
 
 
 $('.closeCart').onclick = function (event) {
-    overlayHide();
     closeCart();
+    overlayHide();
+    canScroll();
     event.stopPropagation();
 }
 
-sortList('A');
-
-product.forEach(function (item) {
-    let category = item.categories.toLowerCase();
-    const card = `
-    <div class="card ${category}">
-    <img src="${item.imgSrc}" onerror="this.src='assets/head_paper.png'">
-    <a><h2> $${item.price} CAD </h2>
-    <h1>${item.name}</h1></a>
-    <button> VIEW </button>
-    </div>
-    `;
-    $('main').innerHTML += card;
-});
+$('.menu').onclick = function () {
+    sideNavOpen();
+    overlayShow();
+    canNotScroll();
+}
 
 for (let i = 0; i < li.length; i++) {
     li[i].onclick = function () {
@@ -201,40 +244,34 @@ for (let i = 0; i < li.length; i++) {
 }
 
 window.onload = function () {
-    filter();
     $('header').style.background = 'linear-gradient(360deg, rgba(0,0,0,0) 0%, ' + selectedColor + ' 100%)';
     $('footer').style.background = 'linear-gradient(360deg, ' + selectedColor + ' 0%, rgba(0,0,0,0) 100%';
+    filter();
 }
 
-$('.menu').onclick = function () {
-    sideNavOpen();
-    overlayShow();
-    canNotScroll();
-}
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function () {
+for (let i = 0; i < cards.length; i++) {
+    cards[i].onclick = function () {
         $('.search').style.zIndex = '0';
-        let selectedProduct = this.parentElement.querySelector('h1').innerHTML;
-        productInfoShow();
-        overlayShow();
-        canNotScroll();
-        product.forEach(function (item) {
-            if (selectedProduct === item.name) {
-                let categories = item.categories.split(' ');
+        let selectedProduct = this.querySelector('h1').innerHTML;
+        product.forEach(product => {
+            if (selectedProduct === product.name) {
+                let categories = product.categories.split(' ');
                 const productInfo = `
                 <svg  onclick='productInfoClose()' class="closeCard" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
                 <path d="M0 0h24v24H0z" fill="none" /></svg>
-                <img src='${item.imgSrc}'>
-                <h2>${item.name}</h2>
-                <h1>$${item.price} CAD</h1>
+                <img src='${product.imgSrc}'>
+                <h2>${product.name}</h2>
+                <h1>$${product.price} CAD</h1>
                 <h5>${categories}</h5>
                 `;
 
                 $('.product-info').innerHTML = productInfo;
             }
         });
+        productInfoShow();
+        overlayShow();
+        canNotScroll();
     }
 }
 
@@ -250,6 +287,12 @@ $('.close').onclick = function (event) {
 
 $('.overlay').onclick = function () {
     overlayHide();
+    searchInputCheck();
+    sideNavClose();
+    productInfoHide();
+    canScroll();
+    loginHide();
+    closeCart();
 }
 
 $('.search').onclick = function () {
@@ -281,11 +324,6 @@ $('.nav-menu .signin').onclick = function () {
 }
 
 
-function productInfoClose() {
-    productInfoHide();
-    canScroll();
-    overlayHide();
-}
 
 function login() {
     const userInput = $('.inputLogin').value.toUpperCase();
@@ -300,30 +338,24 @@ function login() {
                 $('.nav-heading h1.title').innerHTML = user[i].name.first + ' ' + user[i].name.last;
                 $('.nav-heading a h1.email').innerHTML = user[i].email;
                 $('.nav-heading a').href = 'mailto: ${user[i].email}';
+
                 overlayHide();
                 loginHide();
 
-                // $('ul.nav-menu .signin').style.display = 'none';
                 $('ul.nav-menu .signin h1').innerText = 'Account';
                 $('ul.nav-menu .logout').style.display = 'block';
 
                 setTimeout(function () {
                     $('.login-form').style.display = 'none';
                     $('.signup-form').style.display = 'none';
-
                     $('.account-info').style.display = 'block'
-
                 }, 500);
 
                 $('.account-info span.name').innerText = user[i].name.first + ' ' + user[i].name.last;
                 $('.account-info span.mobile').innerText = user[i].mobile;
                 $('.account-info span.email').innerText = user[i].email;
-
                 $('.login h1.LoggedIn').style.display = 'flex';
                 $('.login h1.LoggedOut').style.display = 'none';
-
-
-
             } else {
                 $('.login-form .inputPassword').value = "";
                 $('.login-form .inputPassword').placeholder = "Incorrent Password";
@@ -337,3 +369,67 @@ function login() {
         $('.login-form .inputPassword').value = "";
     }
 }
+
+// let lazyImages = $$('.lazy-img');
+// let inAdvance = 300;
+
+// lazy loading images
+// function lazyLoad() {
+//     lazyImages.forEach(image => {
+//         if (image.offsetTop < window.innerHeight + window.pageYOffset + inAdvance) {
+//             image.src = image.dataset.src;
+//             image.onload = () => image.classList.add('loaded');
+//             console.log(image.src);
+
+//         }
+//     });
+// }
+// lazyLoad();
+
+let counter = 0;
+buttons.forEach(button => {
+    button.onclick = (event) => {
+        const cartItem = `
+        <div class="item">
+            <h1> ${button.parentElement.querySelector('h1').innerText} </h1>
+            <hr>
+            <svg class="removeCartItem" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            <path d="M0 0h24v24H0z" fill="none" /></svg>
+            <h1 class="item-price"> ${button.parentElement.querySelector('h2').innerText} </h1>
+        </div>    
+        `;
+        $('.cart span').style.transform = 'rotate(0deg)'
+        $('.cart').innerHTML += cartItem;
+        counter += 1;
+
+
+        event.stopPropagation();
+        let removeItem = $$('.cart .item .removeCartItem');
+
+        removeItem.forEach(button => {
+            button.onclick = () => {
+
+                button.parentElement.style.transform = 'translateX(-200%)';
+                setTimeout(function () {
+                    button.parentElement.style.display = 'none';
+                }, 400);
+                counter -= 1;
+
+                $('.cart span').innerText = counter;
+            }
+        });
+
+        setTimeout(function () {
+            $('.cart span').style.transform = 'rotate(360deg)'
+        }, 50);
+
+        $('.cart span').innerText = counter;
+
+    }
+});
+
+
+
+
